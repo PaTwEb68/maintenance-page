cfd05b828db5336458f94ad1b5085791f348454a
PaTwEb68 <114364915+PaTwEb68@users.noreply.github.com>
2025-07-03 22:15:47 +0200

    Create index.php
    Salut, de temps en temps il faut faire quelques modifications :-)

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8" />
+  <title>Maintenance</title>
+  <style>
+    body { background:#111; color:#fff; text-align:center; padding-top:10%; font-family:sans-serif; }
+    h1 { font-size:2em; }
+    p { margin-top:1em; }
+  </style>
+</head>
+<body>
+  <h1>🚧 Site en maintenance</h1>
+  <p>patweb68.nohost.me sera de retour très bientôt !<br>Merci pour votre patience.</p>
+</body>
+</html>
